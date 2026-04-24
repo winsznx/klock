028ccd2f9626e0a31f7197efc638dded47ef66b4
winsznx <timjosh507@gmail.com>
2026-04-24 20:03:56 +0100

    refactor(react): introduce Nullable generic utility type

--- packages/pulse-react/src/types.ts
+++ packages/pulse-react/src/types.ts
@@ -54,3 +54,5 @@ export interface PulseAuthContextValue {
 export type DeepReadonly<T> = {
     readonly [P in keyof T]: T[P] extends Record<string, unknown> ? DeepReadonly<T[P]> : T[P];
 };
+
+export type Nullable<T> = T | null;
