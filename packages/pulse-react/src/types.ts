cc6e00a027f15e618a14ecae6373e4671cb90ee3
winsznx <timjosh507@gmail.com>
2026-04-24 20:03:55 +0100

    refactor(react): introduce DeepReadonly generic utility type

--- packages/pulse-react/src/types.ts
+++ packages/pulse-react/src/types.ts
@@ -50,3 +50,7 @@ export interface PulseAuthContextValue {
     storageKey: string
 }
 
+
+export type DeepReadonly<T> = {
+    readonly [P in keyof T]: T[P] extends Record<string, unknown> ? DeepReadonly<T[P]> : T[P];
+};
