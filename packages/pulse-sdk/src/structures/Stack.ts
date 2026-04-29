e392933c312031a0307e360c289e9bca3e800808
winsznx <timjosh507@gmail.com>
2026-04-29 07:22:46 +0100

    feat(sdk): implement strongly typed LIFO Stack structure

---
+++ packages/pulse-sdk/src/structures/Stack.ts
@@ -0,0 +1,6 @@
+export class Stack<T> {
+  private items: T[] = [];
+  push(item: T) { this.items.push(item); }
+  pop(): T | undefined { return this.items.pop(); }
+  get size() { return this.items.length; }
+}
