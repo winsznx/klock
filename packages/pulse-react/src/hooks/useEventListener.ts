70d734034aa0f88c4e76eebd3649a0dd520ef91d
winsznx <timjosh507@gmail.com>
2026-04-29 09:07:48 +0100

    feat(react): implement highly generic useEventListener lifecycle hook

---
+++ packages/pulse-react/src/hooks/useEventListener.ts
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from 'react';
+export function useEventListener(eventName: string, handler: Function, element: any = window) {
+  const savedHandler = useRef<Function>();
+  useEffect(() => { savedHandler.current = handler; }, [handler]);
+  useEffect(() => {
+    if (!element || !element.addEventListener) return;
+    const eventListener = (e: Event) => savedHandler.current!(e);
+    element.addEventListener(eventName, eventListener);
+    return () => element.removeEventListener(eventName, eventListener);
+  }, [eventName, element]);
+}
